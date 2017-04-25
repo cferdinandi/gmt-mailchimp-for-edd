ca20b718ce9e942c088bf3435abab72c153c1098
cferdinandi <chris@gomakethings.com>
2017-04-25 12:33:53 -0400

    v2.0.0 Complete rewrite without dependency on GMT MailChimp and tighter integration with individual downloads.

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # GMT MailChimp for EDD
-Extends [GMT MailChimp](https://github.com/cferdinandi/gmt-mailchimp) with Easy Digital Downloads support. Automatically add people who purchase specific products or use partiular discounts codes to MailChimp lists and interest groups (optional).
+Automatically add people who purchase specific downloads to your MailChimp list. You can also optionally add them to interest groups.
 
-[Download GMT MailChimp](https://github.com/cferdinandi/gmt-mailchimp-for-edd/archive/master.zip)
+[Download GMT MailChimp for EDD](https://github.com/cferdinandi/gmt-mailchimp-for-edd/archive/master.zip)
 
 
 
 ## Getting Started
 
-Getting started with GMT MailChimp is as simple as installing a plugin:
+Getting started with GMT MailChimp for EDD is as simple as installing a plugin:
 
 1. Upload the `gmt-mailchimp-for-edd` folder to the `/wp-content/plugins/` directory.
 2. Activate the plugin through the Plugins menu in WordPress.
@@ -18,11 +18,15 @@ It's recommended that you also install the [GitHub Updater plugin](https://githu
 
 
 
-## Adding a purchaser to a MailChimp list
+## Adding a buyer to a MailChimp list
 
-1. [Create a MailChimp form](https://github.com/cferdinandi/gmt-mailchimp#creating-mailchimp-forms) like you normally would with GMT MailChimp.
-2. Under `Easy Digital Downloads Integration`, select a `Download` (product) or `Discount` (discount code) that will cause the buyer to get added to your list.
+In your download, under `MailChimp`:
+
+1. Check `Subscribe buyers to your list`.
+2. Add a `List ID`.
 3. If you don't want them to receive the double opt-in email, check `Disable double opt-in`.
+4. Check off any desired interest groups.
+5. Click `Publish` or `Update`.
 
 
 
@@ -34,4 +38,4 @@ Please read the [Contribution Guidelines](CONTRIBUTING.md).
 
 ## License
 
-The code is available under the [MIT License](LICENSE.md).
+The code is available under the [GPLv3 License](LICENSE.md).
--- includes/mailchimp.php
+++ includes/mailchimp.php
@@ -2,23 +2,37 @@
 
 	/**
 	 * Add subscriber to MailChimp
-	 * @param  array  $mailchimp  The MailChimp integration data
+	 * @param  array  $details  The MailChimp details
 	 * @param  string $subscriber The subscriber email address
 	 */
-	function mailchimp_edd_add_to_mailchimp( $mailchimp, $subscriber, $no_opt_in ) {
+	function mailchimp_edd_add_to_mailchimp( $details, $subscriber ) {
+
+		// Create interest groups array
+		if ( empty( $details['interests'] ) ) {
+			$interests = new stdClass();
+		} else {
+			$interests = array();
+			foreach ( $details['interests'] as $key => $group ) {
+				$interests[$key] = true;
+			}
+		}
 
 		// Create API call
-		$options = mailchimp_get_theme_options();
-		$shards = explode( '-', $options['mailchimp_api_key'] );
-		$url = 'https://' . $shards[1] . '.api.mailchimp.com/3.0/lists/' . $mailchimp['list_id'] . '/members';
+		$api_key = edd_get_option( 'gmt_mailchimp_edd_api_key', false );
+		$shards = explode( '-', $api_key );
+		$url = 'https://' . $shards[1] . '.api.mailchimp.com/3.0/lists/' . $details['list_id'] . '/members';
 		$params = array(
 			'headers' => array(
-				'Authorization' => 'Basic ' . base64_encode( 'mailchimp' . ':' . $options['mailchimp_api_key'] )
+				'Authorization' => 'Basic ' . base64_encode( 'mailchimp' . ':' . $api_key )
 			),
 			'body' => json_encode(array(
-				'status' => ( $no_opt_in === 'on' ? 'subscribed' : 'pending' ),
-				'email_address' => $subscriber,
-				'interests' => ( !array_key_exists( 'group', $mailchimp ) || empty( $mailchimp['group'] ) ? new stdClass() : array( $mailchimp['group'] => true ) ),
+				'status' => ( $details['disable_optin'] === 'on' ? 'subscribed' : 'pending' ),
+				'email_address' => $subscriber['email'],
+				'merge_fields' => array(
+					'FNAME' => $subscriber['first_name'],
+					'LNAME' => $subscriber['last_name'],
+				),
+				'interests' => $interests,
 			)),
 		);
 
@@ -33,11 +47,15 @@
 			$url .= '/' . md5( $subscriber );
 			$params = array(
 				'headers' => array(
-					'Authorization' => 'Basic ' . base64_encode( 'mailchimp' . ':' . $options['mailchimp_api_key'] )
+					'Authorization' => 'Basic ' . base64_encode( 'mailchimp' . ':' . $api_key )
 				),
 				'method' => 'PUT',
 				'body' => json_encode(array(
-					'interests' => ( !array_key_exists( 'group', $mailchimp ) || empty( $mailchimp['group'] ) ? new stdClass() : array( $mailchimp['group'] => true ) ),
+					'merge_fields' => array(
+						'FNAME' => $subscriber['first_name'],
+						'LNAME' => $subscriber['last_name'],
+					),
+					'interests' => $interests,
 				)),
 			);
 			$request = wp_remote_post( $url, $params );
@@ -62,39 +80,16 @@
 	 * Add buyer to MailChimp after purchase is complete
 	 * @param  Integer $payment_id ID for the purchase
 	 */
-	function mailchimp_edd_on_complete_purchase(  $payment_id  ) {
+	function mailchimp_edd_on_complete_purchase( $payment_id ) {
 
 		// Get purchase data
 		$purchase = edd_get_payment_meta( $payment_id );
 
-		// Get downloads and discounts
-		$downloads = get_posts(
-			array(
-				'posts_per_page'   => -1,
-				'post_type'        => 'gmt-mailchimp',
-				'post_status'      => array( 'publish', 'active' ),
-				'meta_query' => array(
-					'relation' => 'OR',
-					array(
-						'key' => 'mailchimp_edd_download',
-						'value' => $purchase['downloads'][0]['id'],
-						'compare' => '='
-					),
-					array(
-						'key' => 'mailchimp_edd_discount',
-						'value' => $purchase['user_info']['discount'],
-						'compare' => '='
-					)
-				)
-			)
-		);
-
-		// For each integration, add to MailChimp
-		foreach ( $downloads as $key => $download ) {
-			$details = get_post_meta( $download->ID, 'mailchimp_details', true );
-			$no_opt_in = get_post_meta( $download->ID, 'mailchimp_edd_double_opt_in', true );
-			if ( empty( $details['list_id'] ) || empty( $purchase['user_info']['email'] ) ) continue;
-			mailchimp_edd_add_to_mailchimp( $details, $purchase['user_info']['email'], $no_opt_in );
+		// For each download, add subscriber to the list
+		foreach( $purchase['downloads'] as $key => $download ) {
+			$details = get_post_meta( $download['id'], 'mailchimp_edd_details', true );
+			if ( $details['signup'] === 'off' || empty( $details['list_id'] ) || empty( $purchase['user_info']['email'] ) ) continue;
+			$mailchimp = mailchimp_edd_add_to_mailchimp( $details, $purchase['user_info'] );
 		}
 
 	}
